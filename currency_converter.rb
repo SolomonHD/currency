37c02c06cb88e91747e0e7284c639a6f2e07856f
Solomon Hilliard <shillia6@eagles.nccu.edu>
2015-01-20 22:58:15 -0500

    Had stuff working now its not, tried overloading

---
+++ currency_converter.rb
@@ -0,0 +1,43 @@
+class Currency
+#  @amount = 12.01
+#  @currency_code = ""
+  attr_writer :amount, :code
+
+  def initialize (amount, code)
+    @amount = amount
+    @code = code
+  end
+
+  def amount
+    return @amount
+    #puts @currency_code
+  end
+
+  def code
+    return @code
+  end
+
+  def == (currency_being_compared)
+    if self.code == currency_being_compared.code
+      if self.amount == currency_being_compared.amount
+        return true
+      else
+        return false
+      end
+    raise DifferentCurrencyCodeError
+    end
+
+  end
+
+  def - (currency_to_subtract)
+    if self.code  ==  currency_to_subtract.code
+        answer = self.amount - currency_to_subtract.amount
+      #  puts "The two currencies can be subtracted"
+      #  puts "The answer is #{answer}   #{currency1.code1}"
+      return answer
+    else
+        raise DifferentCurrencyCodeError
+    end
+  end
+
+end
---
+++ currency_converter_test.rb
@@ -0,0 +1,53 @@
+require './currency_converter.rb'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class CurrencyConverterTest < Minitest::Test
+
+  def self.test_order
+    :alpha
+  end
+
+  def test_currency_class_exists
+    assert Currency
+  end
+
+  def test_currency_can_be_initalized
+    test_currency = Currency.new(25.34, "USD")
+    assert_equal 25.34, test_currency.amount
+  end
+
+  def test_currency_can_equals
+    usd_amount1 = Currency.new(33.24, "USD")
+    usd_amount2 = Currency.new(33.24, "USD")
+    assert true, usd_amount1 == usd_amount2
+    eur_amt1 == Currency.new(20.50, "EUR")
+    eur_amt2 == Currency.new(22.50, "EUR")
+    assert false, eur_amt1 == eur_amt2
+  end
+
+  def test_currency_can_be_subtracted
+    usd_amount1 = Currency.new(33.24, "USD")
+    usd_amount2 = Currency.new(32.24, "USD")
+    assert_equal 1.0, usd_amount1 - usd_amount2
+  end
+
+  def test_unlike_currency_cannot_subtract
+    usd_amount1 = Currency.new(33.24, "USD")
+    eur_amount2 = Currency.new(32.24, "EUR")
+    assert_raises(DifferentCurrencyCodeError) do
+      usd_amount1 - eur_amount2
+    end
+  end
+
+#  def test_output_method
+#  end
+
+#  def test_currency_can_get_amount_and_code
+#    assert_equal ,
+#  end
+
+#needs to be able to subtract from same currency, so I need two variables for
+#the currencys and a if then for the code
+
+end
